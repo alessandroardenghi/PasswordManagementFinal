f5d79e69bb14b4616c58205b24e9eb513bcafbe8
Alessandro Ardenghi <alessandro.ardenghi@studbocconi.it>
2023-12-25 23:03:09 +0100

    added tutorial view and tutorial button

---
+++ PasswordManagement/Views/LockedFormView.swift
@@ -0,0 +1,120 @@
+//
+//  TutorialViewHelper.swift
+//  PasswordManagement
+//
+//  Created by Alessandro Ardenghi on 25/12/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct LockedFormView: View {
+    let title: String
+    @Binding var variable: String
+    @Environment(\.colorScheme) var colorScheme
+    let secure: Bool
+    var placeholder: String
+    @State var visible = false
+    @State var locked = true
+    @State var change_password = false
+    @State private var length = 8
+    @State var special_characters: Bool = true
+    @State var numbers: Bool = true
+
+    var body: some View {
+        VStack (spacing: 2) {
+            
+            Text(title)
+                .bold()
+                .padding()
+            
+            if secure {
+                
+                if visible {
+                    TextField(placeholder, text: $variable)
+                        .padding()
+                        .disabled(true)
+                        .frame(width: 300, height: 50)
+                        .background(colorScheme == .dark ? Color.white.opacity(0.1) : Color.black.opacity(0.1))
+                        .cornerRadius(10)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                } else {
+                    SecureField(placeholder, text: $variable)
+                        .padding()
+                        .disabled(true)
+                        .frame(width: 300, height: 50)
+                        .background(colorScheme == .dark ? Color.white.opacity(0.1) : Color.black.opacity(0.1))
+                        .cornerRadius(10)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                }
+                
+                HStack(spacing: 25) {
+                    
+                    // BUTTON TO SEE PASSWORD
+                    Button(action: {
+                        visible.toggle()
+                    }) {
+                        Image(systemName: visible ? "eye" : "eye.slash")
+                            .foregroundColor(.white)
+                    }
+                    
+                    .frame(width: 50, height: 30)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    
+                    // BUTTON TO LOCK PASSWORD
+                    Button(action: {
+                        locked.toggle()
+                    }) {
+                        Image(systemName: !locked ? "lock.open" : "lock")
+                            .foregroundColor(.white)
+                    }
+                    
+                    .frame(width: 50, height: 30)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    
+                    // BUTTON TO CHANGE PASSWORD
+                    Button(action: {
+                        change_password.toggle()
+                    }) {
+                        Image(systemName: "arrow.triangle.2.circlepath")
+                            .foregroundColor(.white)
+                    }
+                    
+                    .frame(width: 50, height: 30)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    
+                    // BUTTON TO COPY PASSWORD
+                    Button(action: {
+                        UIPasteboard.general.string = variable
+                    }) {
+                        Image(systemName: "square.on.square")
+                            .foregroundColor(.white)
+                    }
+                    
+                    .frame(width: 50, height: 30)
+                    .background(.blue)
+                    .cornerRadius(10)
+                }
+            } else {
+                TextField(placeholder, text: $variable)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .disabled(true)
+                    .background(colorScheme == .dark ? Color.white.opacity(0.1) : Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+            }
+            
+        }
+        
+    }
+    
+}
+
+
---
+++ PasswordManagement/Views/TutorialView.swift
@@ -0,0 +1,8 @@
+//
+//  TutorialView.swift
+//  PasswordManagement
+//
+//  Created by Alessandro Ardenghi on 25/12/23.
+//
+
+import Foundation
