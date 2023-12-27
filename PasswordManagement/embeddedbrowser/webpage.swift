137e29b545bb1e57ec91560f677903ae696a646e
Alessandra Bontempi <133512150+Ale202317@users.noreply.github.com>
2023-12-27 12:16:45 +0100

    Update webpage.swift

--- PasswordManagement/embeddedbrowser/webpage.swift
+++ PasswordManagement/embeddedbrowser/webpage.swift
@@ -6,11 +6,12 @@
 //
 
 import SwiftUI
-import Foundation
 import WebKit
 
 struct WebView: UIViewRepresentable {
     var url: URL
+    var username: String
+    var password: String
 
     func makeUIView(context: Context) -> WKWebView {
         return WKWebView()
@@ -19,5 +20,31 @@ struct WebView: UIViewRepresentable {
     func updateUIView(_ uiView: WKWebView, context: Context) {
         let request = URLRequest(url: url)
         uiView.load(request)
+
+        uiView.navigationDelegate = context.coordinator
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self, username: username, password: password)
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebView
+        var username: String
+        var password: String
+
+        init(_ parent: WebView, username: String, password: String) {
+            self.parent = parent
+            self.username = username
+            self.password = password
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            // fill in credentials
+            let fillCredentials = "document.getElementById('loginForm').querySelector('[name=username]').value='\(username)';document.getElementById('loginForm').querySelector('[name=password]').value='\(password)';"
+            
+            // the user will have to manually take care of pop ups since selectors are different from page to page, and one XPATH wouldn't work for all pages
+            webView.evaluateJavaScript(fillCredentials, completionHandler: nil)
+        }
     }
 }
