75fc30b410f5bbafb7cee70cac2733d3e98f0b6c
Alessandra Bontempi <133512150+Ale202317@users.noreply.github.com>
2024-01-03 12:29:21 +0100

    Update DetailView.swift

--- PasswordManagement/Views/DetailView.swift
+++ PasswordManagement/Views/DetailView.swift
@@ -9,17 +9,10 @@ import Foundation
 import SwiftUI
 import SwiftData
 
-// protocol used for web automation --> triggered when an action is called
-protocol WebAutomationDelegate {
-    func performAction(username: String, password: String, url: String, actionType: ActionType)
-}
-// callable actions by the user --> these are the buttons the user sees and that trigger
-// our protocol. if the automation fails, we have a backup case where the user manually
-// enters all the data
-enum ActionType {
-    case changePassword
-    case deleteAccount
-    case deleteSubscription
+enum ActionType: String {
+    case changePassword = "change_password"
+    case deleteAccount = "delete_account"
+    case deleteSubscription = "delete_subscription"
 }
 
 struct DetailView: View {
@@ -28,30 +21,30 @@ struct DetailView: View {
     @State var variable: LoginInfoItem
     @State private var showWebView = false
     @State var extras: Bool = false
-    var automationDelegate: WebAutomationDelegate?
-    
+    @State private var selectedAction: ActionType?
+
     var body: some View {
         ScrollView {
-            VStack (spacing: 2){
+            VStack (spacing: 2) {
                 Text("Login Info")
                     .bold()
                     .font(.system(size: 30))
                     .padding()
-                
-                NewFormView(title: "Website", variable: $variable.website, secure: false, placeholder: "Enter website")
-                
-                NewFormView(title: "Email", variable: $variable.email, secure: false, placeholder: "Enter email")
-                
-                NewFormView(title: "Weblink", variable: $variable.weblink, secure: false, placeholder: "https://yourwebsite.com")
-                
-                NewFormView(title: "Username", variable: $variable.username, secure: false, placeholder: "Enter username")
-                
-                NewFormView(title: "Password", variable: $variable.password, secure: true, placeholder: "Enter password")
-                
+
+                NewFormView(title: "Website", variable: $variable.website, secure: false, placeholder: "Enter website", uuid: $variable.id)
+
+                NewFormView(title: "Email", variable: $variable.email, secure: false, placeholder: "Enter email", uuid: $variable.id)
+
+                NewFormView(title: "Weblink", variable: $variable.weblink, secure: false, placeholder: "https://yourwebsite.com", uuid: $variable.id)
+
+                NewFormView(title: "Username", variable: $variable.username, secure: false, placeholder: "Enter username", uuid: $variable.id)
+
+                NewFormView(title: "Password", variable: $variable.password, secure: true, placeholder: "Enter password", uuid: $variable.id)
+
                 Text("Subscription")
                     .bold()
                     .padding()
-                
+
                 Picker("Select", selection: $variable.subscription) {
                     Text("Yes").tag(true)
                     Text("No").tag(false)
@@ -59,7 +52,7 @@ struct DetailView: View {
                 .pickerStyle(SegmentedPickerStyle())
                 .padding()
                 .frame(maxWidth: 320)
-                
+
                 if variable.subscription {
                     Text("Select End Date")
                         .bold()
@@ -70,19 +63,23 @@ struct DetailView: View {
                         .frame(width: 100)
 
                     Spacer()
-                    
+
                     Button(action: {
-                                        performWebAction(actionType: .deleteSubscription)
-                                    }) {
-                                        Text("Delete Subscription")
-                                            .frame(width: 250, height: 30)
-                                            .padding()
-                                            .foregroundColor(.white)
-                                            .background(Color.blue)
-                                            .cornerRadius(10)
-                                    }
+                        selectedAction = .deleteSubscription
+                        performAction(action: .deleteSubscription)
+                    }) {
+                        Text("Delete subscription")
+                            .frame(width: 250, height: 30)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .sheet(isPresented: $showWebView) {
+                        WebView(url: URL(string: variable.weblink)!, username: variable.username, password: variable.password, oldPassword: variable.oldPassword, action: selectedAction)
+                    }
                 }
-                
+
                 Text("Extras")
                     .bold()
                     .padding()
@@ -93,57 +90,115 @@ struct DetailView: View {
                 .pickerStyle(SegmentedPickerStyle())
                 .padding()
                 .frame(maxWidth: 320)
-                
+
                 if extras {
                     VStack(alignment: .leading) {
                         CheckBoxView(background: .blue, variable: $variable.full_name, text: "Full Name")
-                        
+
                         CheckBoxView(background: .blue, variable: $variable.address, text: "Address")
-                        
+
                         CheckBoxView(background: .blue, variable: $variable.credit_card, text: "Credit Card")
-                        
+
                         CheckBoxView(background: .blue, variable: $variable.date_of_birth, text: "Date of Birth")
-                        
                     }
                 }
-                
+
                 Button(action: {
-                            showWebView = true
-                            performWebAction(actionType: .changePassword)
-                        }) {
-                            Text("Change Password")
-                                .frame(width: 250, height: 30)
-                                .padding()
-                                .foregroundColor(.white)
-                                .background(Color.blue)
-                                .cornerRadius(10)
-                        }
-                        .sheet(isPresented: $showWebView) {
-                            if let url = URL(string: variable.weblink) {
-                                WebView(url: url, username: variable.username, password: variable.password)
-                            }
-                        }
-                
-                Spacer()
-                
-                Button(action: {
-                                    performWebAction(actionType: .deleteAccount)
+                                    selectedAction = .changePassword
+                                    performAction(action: .changePassword)
                                 }) {
-                                    Text("Delete Account")
+                                    Text("Change Password")
                                         .frame(width: 250, height: 30)
                                         .padding()
                                         .foregroundColor(.white)
                                         .background(Color.blue)
                                         .cornerRadius(10)
                                 }
+                                .sheet(isPresented: $showWebView) {
+                                    WebView(url: URL(string: variable.weblink)!, username: variable.username, password: variable.password, oldPassword: variable.oldPassword, action: selectedAction)
+                                }
+
+                Spacer()
+
+                Button(action: {
+                    selectedAction = .deleteAccount
+                    performAction(action: .deleteAccount)
+                }) {
+                    Text("Delete account")
+                        .frame(width: 250, height: 30)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .sheet(isPresented: $showWebView) {
+                    WebView(url: URL(string: variable.weblink)!, username: variable.username, password: variable.password, oldPassword: variable.oldPassword, action: selectedAction)
+                }
+                .padding(20)
             }
-            .padding(20)
         }
     }
-    private func performWebAction(actionType: ActionType) {
-                automationDelegate?.performAction(username: variable.username,
-                                                  password: variable.password,
-                                                  url: variable.weblink,
-                                                  actionType: actionType)
+
+    private func performAction(action: ActionType) {
+        var parameters = ["username": variable.username,
+                          "url": variable.weblink,
+                          "action": action.rawValue]
+        
+        if action == .changePassword {
+            parameters["oldPassword"] = variable.oldPassword
+            parameters["password"] = variable.password
+        } else {
+            parameters["password"] = variable.password
+        }
+
+        let endpoint: String
+        switch action {
+        case .changePassword:
+            endpoint = "http://127.0.0.1:5000/change_password"
+        case .deleteAccount:
+            endpoint = "http://127.0.0.1:5000/delete_account"
+        case .deleteSubscription:
+            endpoint = "http://127.0.0.1:5000/delete_subscription"
+        }
+
+        sendPostRequest(url: endpoint, parameters: parameters) { result in
+            switch result {
+            case .success(let responseStr):
+                print("Response: \(responseStr)")
+                showWebView = true
+            case .failure(let error):
+                print("Error: \(error.localizedDescription)")
             }
+        }
+    }
+}
+
+func sendPostRequest(url: String, parameters: [String: Any], completion: @escaping (Result<String, Error>) -> Void) {
+    guard let url = URL(string: url) else {
+        print("Invalid URL")
+        return
+    }
+
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+    do {
+        request.httpBody = try JSONSerialization.data(withJSONObject: parameters)
+    } catch {
+        completion(.failure(error))
+        return
+    }
+
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            completion(.failure(error))
+            return
+        }
+        if let data = data, let responseStr = String(data: data, encoding: .utf8) {
+            completion(.success(responseStr))
+        } else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
+        }
+    }.resume()
 }
