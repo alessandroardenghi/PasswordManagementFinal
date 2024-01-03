70f074b36fd6b43eb8a719c4d7bcad408a75ddde
Alessandra Bontempi <133512150+Ale202317@users.noreply.github.com>
2024-01-03 12:30:59 +0100

    Update webpage.swift

--- PasswordManagement/embeddedbrowser/webpage.swift
+++ PasswordManagement/embeddedbrowser/webpage.swift
@@ -12,6 +12,8 @@ struct WebView: UIViewRepresentable {
     var url: URL
     var username: String
     var password: String
+    var oldPassword: String
+    var action: ActionType?
 
     func makeUIView(context: Context) -> WKWebView {
         return WKWebView()
@@ -25,26 +27,45 @@ struct WebView: UIViewRepresentable {
     }
 
     func makeCoordinator() -> Coordinator {
-        Coordinator(self, username: username, password: password)
+        Coordinator(self, username: username, password: password, oldPassword: oldPassword, action: action)
     }
 
     class Coordinator: NSObject, WKNavigationDelegate {
         var parent: WebView
         var username: String
         var password: String
+        var oldPassword: String
+        var action: ActionType?
 
-        init(_ parent: WebView, username: String, password: String) {
+        init(_ parent: WebView, username: String, password: String, oldPassword: String, action: ActionType?) {
             self.parent = parent
             self.username = username
             self.password = password
+            self.oldPassword = oldPassword
+            self.action = action
         }
 
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            // fill in credentials
-            let fillCredentials = "document.getElementById('loginForm').querySelector('[name=username]').value='\(username)';document.getElementById('loginForm').querySelector('[name=password]').value='\(password)';"
-            
-            // the user will have to manually take care of pop ups since selectors are different from page to page, and one XPATH wouldn't work for all pages
-            webView.evaluateJavaScript(fillCredentials, completionHandler: nil)
+            // Inject JavaScript based on the action
+            switch action {
+            case .changePassword:
+                // JavaScript code to change the password
+                break
+            case .deleteAccount:
+                // JavaScript code to delete the account
+                break
+            case .deleteSubscription:
+                // JavaScript code to delete the subscription
+                break
+            default:
+                // JavaScript code to login
+                let loginScript = """
+                document.querySelector('#usernameField').value = '\(username)';
+                document.querySelector('#passwordField').value = '\(password)';
+                document.querySelector('#loginButton').click();
+                """
+                webView.evaluateJavaScript(loginScript, completionHandler: nil)
+            }
         }
     }
 }
